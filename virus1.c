dd138e916705b903112cbf2e48f8064943098756
Daltro-Oliveira-Vinuto <daltroov777@gmail.com>
2016-07-08 10:09:40 -0300

    Add files via upload
    virus

---
+++ virus1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
+void replicate(void) {
+	int i;
+	char alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+	srand(time(NULL));
+
+	for(i=0;i<1e3;i++) {
+		printf("%c",alphabet[rand()%26]);
+
+	}
+
+}
+
+int main(void) {
+
+	while(1) {
+		replicate();
+		
+	}
+	return 0;
+}
